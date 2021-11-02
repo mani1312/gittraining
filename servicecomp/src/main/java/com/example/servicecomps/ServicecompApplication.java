b60b088fab35bb8e681ba2bcdbcf12093aa4ad2d
mani1312 <89628974+mani1312@users.noreply.github.com>
2021-11-02 16:01:14 +0530

    service components
    codes

---
+++ servicecomp/src/main/java/com/example/servicecomps/ServicecompApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicecomps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicecompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicecompApplication.class, args);
+	}
+
+}
---
+++ servicecomp/src/test/java/com/example/servicecomps/ServicecompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicecomps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicecompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
